5d19a4daf66c21cff46f25805161b6469574e2b5
anagri <127566+anagri@users.noreply.github.com>
2024-03-12 18:32:43 +0530

    [Amir] having homebrew tap for bodhisearch apps, having formula for installing bodhiserver

---
+++ README.md
@@ -0,0 +1,14 @@
+# Homebrew for BodhiSearch apps
+
+This repo contains up-to-date formulas to install BodhiSearch apps using Homebrew.
+
+# Install BodhiServer
+
+1. Add `BodhiSearch/apps` as a tap into homebrew
+  ```shell
+  brew tap BodhiSearch/apps
+  ```
+2. Install `BodhiServer`
+  ```shell
+  brew install bodhiserver
+  ```
---
+++ bodhiserver.rb
@@ -0,0 +1,41 @@
+class Bodhiserver < Formula
+  desc "Run GenerativeAI models locally and access via OpenAI compatible API"
+  homepage "https://github.com/BodhiSearch/BodhiServer"
+  url "https://github.com/BodhiSearch/BodhiServer.git"
+  tag: "v0.1.0"
+  revision: "b4c959c4513fc59e13013cf47b8c1f641114934a"
+  license "MIT"
+  head "https://github.com/BodhiSearch/BodhiServer.git", branch: "main"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "build", "--release"
+    bin.install "target/release/bodhiserver"
+  end
+
+  service do
+    run [opt_bin/"bodhiserver", "serve"]
+    keep_alive true
+    working_dir var
+    log_path var/"log/bodhiserver.log"
+    error_log_path var/"log/bodhiserver.log"
+  end
+
+  test do
+    port = free_port
+    ENV["BODHISERVER_PORT"] = "#{port}"
+    pid = fork { exec "#{bin}/bodhiserver", "serve" }
+    sleep 1
+    begin
+      assert_match "pong", shell_output("curl -s localhost:#{port}/ping")
+    ensure
+      Process.kill "SIGTERM", pid
+    end
+  end
+end
